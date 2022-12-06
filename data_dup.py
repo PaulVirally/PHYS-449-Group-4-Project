1aec4f3665226e4fa80a384c1c85cf878dbbee90
Pojoe71 <67134728+Pojoe71@users.noreply.github.com>
2022-12-06 18:59:59 -0500

    Working oversampling for 3FGL

--- data_dup.py
+++ data_dup.py
@@ -1,9 +1,9 @@
 import numpy as np
-import argparse, random
+import argparse, random, os
 from astropy.io import fits
 
-def data_dup(infile):
-    global agn_mask, pulsar_mask, classes, duplicated
+def data_dup(hdul_fgl, outfile):
+    global agn_mask, pulsar_mask, classes, data
     classes = hdul_fgl[1].data['CLASS1']
     agn_classes = ['psr  ', 'agn  ', 'FSRQ ', 'fsrq ', 'BLL  ', 'bll  ', 'BCU  ', 'bcu  ', 'RDG  ', 'rdg  ', 'NLSY1', 'nlsy1', 'ssrq ', 'sey  ']
     pulsar_classes = ['PSR  ', 'psr  ']
@@ -11,23 +11,71 @@ def data_dup(infile):
     agn_mask = np.isin(classes, agn_classes)
     pulsar_mask = np.isin(classes, pulsar_classes)
     noclass_mask = classes == no_class
+    bad_data_mask = hdul_fgl[1].data['Signif_Curve'] == 0.0
 
     oversample = sum(agn_mask) - sum(pulsar_mask)
 
-    duplicated = []
-    for i in range(classes.size):
-        duplicated.append(hdul_fgl[1].data[i])
+    nrows1 = hdul_fgl[1].data.shape[0]
+    nrows = nrows1 + oversample
+    hdu = fits.BinTableHDU.from_columns(hdul_fgl[1].columns, nrows=nrows)
 
-    pulsars = []
-    for i in range(classes.size):
-        #pulsars (classification labels in 3FGL: PSR, psr)
-        if hdul_fgl[1].data[i]['CLASS1'] == 'PSR' or hdul_fgl[1].data[i]['CLASS1'] == 'psr':
-            pulsars.append(hdul_fgl[1].data[i])
+    pulsars = hdul_fgl[1].data[pulsar_mask & ~bad_data_mask]
 
-    for i in range(oversample):
-        duplicated.append(random.choice(pulsars))
+    for colname in hdul_fgl[1].columns.names:
+        hdu.data[colname][nrows1:] = random.choice(pulsars[colname])
 
-    duplicated = np.array(duplicated, dtype=object)
+    hdu.writeto('temptable.fits', overwrite=True)
+    hdul_v2 = fits.open('temptable.fits')
+
+    classes = hdul_v2[1].data['CLASS1']
+    agn_mask = np.isin(classes, agn_classes)
+    pulsar_mask = np.isin(classes, pulsar_classes)
+    noclass_mask = classes == no_class
+    bad_data_mask = hdul_v2[1].data['Signif_Curve'] == 0.0
+
+    data = hdul_v2[1].data[(agn_mask | pulsar_mask) & ~bad_data_mask] # data is both pulsars and AGNs
+    pulsars = hdul_v2[1].data[pulsar_mask & ~bad_data_mask]
+    agns = hdul_v2[1].data[agn_mask & ~bad_data_mask]
+    
+    # The easy ones
+    glat = data['GLAT']
+    glon = data['GLON']
+    ln_energy_flux100 = np.log(data['Energy_Flux100'])
+    ln_unc_energy_flux100 = np.log(data['Unc_Energy_Flux100'])
+    ln_signif_curve = np.log(data['Signif_Curve'])
+    ln_var_index = np.log(data['Variability_Index'])
+
+    # Hardness ratios
+    ef1 = data['Flux100_300']
+    ef2 = data['Flux300_1000']
+    ef3 = data['Flux1000_3000']
+    ef4 = data['Flux3000_10000']
+    ef5 = data['Flux10000_100000']
+    hr12 = (ef2 - ef1) / (ef2 + ef1)
+    hr23 = (ef3 - ef2) / (ef3 + ef2)
+    hr34 = (ef4 - ef3) / (ef4 + ef3)
+    hr45 = (ef5 - ef4) / (ef5 + ef4)
+
+    # 500 MeV index
+    alpha = data['Spectral_Index']
+    beta = data['beta']
+    gamma = data['Spectral_Index']
+    b = data['Exp_Index']
+    E_c = data['Cutoff'] # In MeV
+    E_0 = data['Pivot_Energy'] # In MeV
+    mev_500_index = np.zeros(data.shape)
+    for i, point in enumerate(data):
+        if point['SpectrumType'] in ['PowerLaw', 'PLExpCutoff', 'PLSuperExpCutoff']:
+            if b[i] == float('-inf'):
+                b[i] = 1
+            mev_500_index[i] = gamma[i] + b[i] * (500 / E_c[i])**b[i]
+        else:
+            mev_500_index[i] = alpha[i] + 2*beta[i] * np.log(500 / E_0[i])
+            
+    in_data = np.vstack((glat, glon, ln_energy_flux100, ln_unc_energy_flux100, ln_signif_curve, ln_var_index, hr12, hr23, hr34, hr45, mev_500_index))
+    out_data = np.isin(data['CLASS1'], agn_classes).astype(float)
+    np.savez_compressed(outfile, in_data=in_data, out_data=out_data)
+
     
 
 if __name__ == '__main__':
@@ -40,9 +88,6 @@ if __name__ == '__main__':
     
     hdul_fgl = fits.open(args.data)
     
-    data_dup(hdul_fgl)
-    np.save(args.outfile, duplicated)
+    data_dup(hdul_fgl, args.outfile)
     
-    print(f'AGN: {sum(agn_mask)}, Pulsar: {sum(pulsar_mask)}, Difference: {sum(agn_mask) - sum(pulsar_mask)}')
-    print(f'Data size: {classes.size}, Duplicated data size: {len(duplicated)}')
     print("Oversampling complete")
